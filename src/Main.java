760dddadc2e62a58f083437d031a085536d6c2be
William Kotesky <williamkotesky@gmail.com>
2023-11-08 15:41:29 -0300

    data processing and casting

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,29 @@ public class Main {
 		
 		System.out.printf("Products:%n%s, which price is $ %.2f%n%s, which price is $ %.2f%n %nRecord: %d years old, code %d and gender: %c%n %nMeasure with eight decimal places: %.8f%nRounded (three decimal places): %.3f%n", product1, price1, product2, price2, age, code, gender, measure, measure);
 		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f", measure);
+		System.out.printf("US decimal point: %.3f%n", measure);
+		
+		
+		//data processing and casting
+		
+		//good practice
+		double doubleVariable = 4.0;
+		float floatVariable = 4f;
+		System.out.printf("%f, %f%n", doubleVariable, floatVariable);
+		
+		//casting: value explicit conversion 
+		int m,n;
+		m=5;
+		n=4;
+		double q = (double) m/n; //forces the division not to round values, preventing losing data
+		System.out.println(q);
+		
+		int r;
+		double s;
+		s=9.4;
+		r = (int) s; //says to compiler that doesn't matter losing data (0.4)
+		System.out.println(r);
+		
 		
 	}
 
