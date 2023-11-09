ad92086f776c36695f4acaa4721e17715e5458a6
William Kotesky <williamkotesky@gmail.com>
2023-11-09 16:54:21 -0300

    Sequential Strucutre Exercises 1

---
+++ src/SequentialStructureExercises.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+//import java.util.Locale;
+
+public class SequentialStructureExercises {
+
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		/*
+		 1) Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+		 	
+		int number1, number2, sum1;
+		number1 = sc.nextInt();
+		number2 = sc.nextInt();
+		sum1 = number1 + number2;
+		
+		System.out.println("SOMA = " + sum1);
+		
+		sc.close();
+		*/
+		
+		/*
+		 2) Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+		 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double radius, area;	
+		radius = sc.nextDouble();
+		
+		area= Math.PI*Math.pow(radius, 2);
+		
+		System.out.printf("A=%.4f", area);
+		
+		sc.close();
+		*/
+		
+		/*
+		 3) Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+		 
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, d, diferenca;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+		
+		sc.close();
+		
+		diferenca = a*b - c*d;
+		
+		System.out.println("DIFERENCA = " +  diferenca);
+		*/
+		
+		/*
+		 4) Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+decimais.
+		 */
+		
+		int employeeNumber,  workedHours;
+		double hourlyRate;
+		
+		
+		
+		
+		
+		
+	}
+
+}
