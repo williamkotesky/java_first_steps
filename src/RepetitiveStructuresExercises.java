1544a24b177708c592f089f4f322697d3af49b8b
William Kotesky <williamkotesky@gmail.com>
2023-11-12 17:02:19 -0300

    For Structure Exercises1

--- src/RepetitiveStructuresExercises.java
+++ src/RepetitiveStructuresExercises.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class RepetitiveStructuresExercises {
@@ -111,25 +112,89 @@ exemplo.
 		//for exercises
 		
 		/*
-		 1)
+		 1) Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+X, se for o caso.
+		 
+		Scanner sc = new Scanner(System.in);
+		int x;
+
+		x = sc.nextInt();
+		sc.close();
+		for(int i = 0; i < x; i++) {
+			if((i+1)%2 != 0) System.out.println(i+1);
+		}
+ 		*/
+		
+		/*
+		 2) Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).
+		 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		sc.close();
+		
+		int valuesAmountIn, valuesAmountOut;
+		valuesAmountIn = 0;
+		valuesAmountOut = 0;
+		
+		for(int i = 0; i < n; i++) {
+			if(i+1 <= 20 && i+1 >= 10) {
+				valuesAmountIn ++;
+			} else {
+				valuesAmountOut ++;
+			}
+		}
+		System.out.println(valuesAmountIn + " in");
+		System.out.println(valuesAmountOut + " out");
+		*/
+
+		/*
+		 3) Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+peso 5.
+		 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		double number1, number2, number3, weightedAverage;
+		
+		for(int i = 0; i < n; i++) {
+			number1 = sc.nextDouble();
+			number2 = sc.nextDouble();
+			number3 = sc.nextDouble();
+			
+			weightedAverage = (2*number1 + 3*number2 + 5*number3)/10;
+			System.out.printf("%.1f%n", weightedAverage);
+			
+		}
+		sc.close();
+		*/
+		
+		/*
+		 4) Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+		 */
+		
+		
+		
+		
+		
+		/*
+		 5) Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+Lembrando que, por definição, fatorial de 0 é 1.
 		 */
 		/*
-		 2)
+		 6) Ler um número inteiro N e calcular todos os seus divisores.
 		 */
 		/*
-		 3)
-		 */
-		/*
-		 4)
-		 */
-		/*
-		 5)
-		 */
-		/*
-		 6)
-		 */
-		/*
-		 7)
+		 7) Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+exemplo.
 		 */
 		
 		
