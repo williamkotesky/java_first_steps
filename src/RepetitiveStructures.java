bc1ac4a5ba552d7fa9ba3910506ef598622ae535
William Kotesky <williamkotesky@gmail.com>
2023-11-13 08:25:38 -0300

    Do-While Structure

--- src/RepetitiveStructures.java
+++ src/RepetitiveStructures.java
@@ -1,10 +1,11 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class RepetitiveStructures {
 
 	public static void main(String[] args) {
 		// while
-		Scanner sc =  new Scanner(System.in);
+		//Scanner sc =  new Scanner(System.in);
 		
 		/*int x, sum;
 		sum = 0;
@@ -20,7 +21,7 @@ public class RepetitiveStructures {
 		// || logical operator, if only one of the expression is TRUE then while will continue.
 		
 		//for
-		
+		/*
 		int n, sum2, number;
 		sum2 = 0;
 		
@@ -32,8 +33,32 @@ public class RepetitiveStructures {
 		}
 		sc.close();
 		System.out.println(sum2);
-		
+		*/
 		//its good to declare the "i" on the for because it will only exists in the execution for time
+		
+		//do while
+		//the condition is verify at the end, that means that its executed at least one time
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double celciusTemperature, fahrenheitTemperature;
+		char keepQuestion;
+		
+		do {
+			System.out.println("Digite a temperatura em Celcius: ");
+			celciusTemperature = sc.nextDouble();
+			fahrenheitTemperature = (9.0*celciusTemperature/5.0) + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", fahrenheitTemperature);
+			System.out.println("Deseja repetir (s/n)?");
+			keepQuestion = sc.next().charAt(0);
+		} while (keepQuestion == 's');
+		
+		sc.close();
+		
+		
+		
+		
 		
 		
 		
