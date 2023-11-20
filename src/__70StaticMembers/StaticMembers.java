4d56fd93f97e17410d3e99697a2770d659e4a986
William Kotesky <williamkotesky@gmail.com>
2023-11-20 17:54:13 -0300

    Statics members 1

---
+++ src/__70StaticMembers/StaticMembers.java
@@ -0,0 +1,13 @@
+package __70StaticMembers;
+
+public class StaticMembers {
+
+	public static void main(String[] args) {
+		//also called class members
+		//oposition of instance members
+		//they dont need an object to be called, just the name of the class
+		//e.g. Math.sqrt and
+		//a class that just has static members is statics and its impossible create instances through it
+	}
+
+}
