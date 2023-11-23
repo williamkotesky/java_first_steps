38b615590beb791e3f1eb995704f175421e0e180
William Kotesky <williamkotesky@gmail.com>
2023-11-23 15:25:35 -0300

    static members exercise

---
+++ src/__70StaticMembers/CurrencyConverter.java
@@ -0,0 +1,8 @@
+package __70StaticMembers;
+
+public class CurrencyConverter {
+	public static double dollarReaisConverter(double dollarPrice, double dollarAmount) {
+		return dollarPrice*dollarAmount + dollarAmount*0.06;
+		
+	}
+}
--- src/__70StaticMembers/StaticMembers.java
+++ src/__70StaticMembers/StaticMembers.java
@@ -1,5 +1,8 @@
 package __70StaticMembers;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class StaticMembers {
 
 	public static void main(String[] args) {
@@ -8,12 +11,26 @@ public class StaticMembers {
 		//they dont need an object to be called, just the name of the class
 		//e.g. Math.sqrt and
 		//a class that just has static members is statics and its impossible create instances through it
-		//inside a class that is static, you can call a method that is not static
+		//inside a method that is static, you can't call a method that is not static
 		
-		double result = Mat.power(2, 8);
-		System.out.println(result);
-		//in this example, a static method power in the class Mat was called, there isn't no need which that method was not static, all objects from it would will be the same.
-
+//		double result = Mat.power(2, 8);
+//		System.out.println(result);
+		//in this example, a static method power in the class Mat was called, there isn't no need that that method is not static, all objects from it would will be the same.
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("What is the dollar price?");
+		double dollarPrice = sc.nextDouble();
+		
+		System.out.println("How many dollars will be bought?");
+		double dollarAmount = sc.nextDouble();
+		
+		double reaisAmount = CurrencyConverter.dollarReaisConverter(dollarPrice, dollarAmount);
+		
+		System.out.println("Amount to be paid in reais:");
+		System.out.println(reaisAmount);
+		
 	}
 
 }
